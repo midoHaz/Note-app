02740ef56a5ca8055f021b5eb3c4bd72c9a99edf
midohamza22 <91686000+midoHaz@users.noreply.github.com>
2023-11-27 11:30:44 +0200

    create notesItems

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:note_app/widgets/custom_app_bar.dart';
 
@@ -6,13 +8,35 @@ class NotesViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 24),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         children: [
-          SizedBox(height: 60,),
-          CustomAppBar(),
+          const SizedBox(height: 60,),
+          const CustomAppBar(),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(25),
+            child:BackdropFilter(filter: ImageFilter.blur(sigmaX: 15,sigmaY: 15),
+            child: Container(
+              height: 250,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+                color: Colors.black45,
+              ),
+              child:Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [ListTile(
+                  title:const  Text("FLUTTER TIPS",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
+                  subtitle:const Text("3 videos in playlist every Flutter widget",style: TextStyle(color: Colors.white),),
+                  trailing:IconButton(onPressed: (){}, icon: const Icon(Icons.delete)) ,
+                ),
 
+                  const Text("27 Nov ,2023",style: TextStyle(color: Colors.white),),
+                ],
+              ),
+            ),) ,
+          )
         ],
       ),
     );
