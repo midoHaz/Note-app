88fd0287cc34aa3a9490d5ee95aafc4ec8ca80e4
midohamza22 <91686000+midoHaz@users.noreply.github.com>
2024-02-06 20:30:19 +0200

    colors list done

--- lib/widgets/color_item.dart
+++ lib/widgets/color_item.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({Key? key}) : super(key: key);
-
+  const ColorItem({Key? key, required this.isSelected, required this.color}) : super(key: key);
+  final bool isSelected;
+  final Color color;
   @override
   Widget build(BuildContext context) {
-    return const CircleAvatar(
-      backgroundColor: Colors.blueAccent,
+    return isSelected?CircleAvatar(
+      backgroundColor: Colors.white,
+      radius: 40.0,
+      child: CircleAvatar(
+        backgroundColor: color,
+        radius: 35.0,
+      ),
+    ): CircleAvatar(
+      backgroundColor:color,
       radius: 40.0,
     );
   }
--- lib/widgets/color_list.dart
+++ lib/widgets/color_list.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/constants.dart';
 import 'package:note_app/widgets/color_item.dart';
 
-class ColorList extends StatelessWidget {
-  const ColorList({Key? key}) : super(key: key);
+class ColorList extends StatefulWidget {
+  const ColorList({
+    Key? key,
+  }) : super(key: key);
 
+  @override
+  State<ColorList> createState() => _ColorListState();
+}
+
+class _ColorListState extends State<ColorList> {
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 40 * 2,
       child: ListView.separated(
-        itemCount: 10,
+        itemCount: kColors.length,
         scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) => const ColorItem(),
+        itemBuilder: (context, index) => GestureDetector(
+            onTap: () {
+              currentIndex = index;
+              setState(() {});
+            },
+            child: ColorItem(
+              color: kColors[index],
+              isSelected: currentIndex == index,
+            )),
         separatorBuilder: (BuildContext context, int index) {
-          return const SizedBox(width: 5,);
-        },),
+          return const SizedBox(
+            width: 5,
+          );
+        },
+      ),
     );
   }
 }
